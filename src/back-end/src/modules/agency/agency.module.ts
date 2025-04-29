7b16291370dd09027cac48c160c015cd36340f6f
sieujs <gamefun206@gmail.com>
2025-04-29 20:00:54 +0700

    update xem

--- src/back-end/src/modules/agency/agency.module.ts
+++ src/back-end/src/modules/agency/agency.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AgencyService } from './service/agency.service';
 import { AgencyController } from './controller/agency.controller';
+import { CommonModule } from '../common';
 
 
 @Module({
+  imports : [CommonModule],
   providers: [AgencyService],
   controllers: [AgencyController]
 })
--- src/back-end/src/modules/agency/controller/agency.controller.ts
+++ src/back-end/src/modules/agency/controller/agency.controller.ts
@@ -1,5 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { AgencyService } from '../service/agency.service';
+import { AgencyParams } from '../models/agency.params';
+import { ApiResponse } from '@nestjs/swagger';
+import { AgencyDto } from '../models/agency.dto';
 
 @Controller('agency')
 export class AgencyController {
+    constructor(
+        private readonly agencyService: AgencyService, // Assuming you have an AgencyService to handle business logic
+    ) {}
+
+    @Get('list')
+    @ApiResponse({
+        type : AgencyDto,
+        isArray : true,
+    })
+    getAgencyList(@Query() params : AgencyParams) {
+        return this.agencyService.getListAngencies(params);
+    }
 }
