2f6e0d2a3bdd9d6c4cfddb91b18c499a12106534
sieujs <gamefun206@gmail.com>
2025-05-05 16:33:19 +0700

    chore: fix partial type of agency

--- src/back-end/src/modules/agency/models/agency.params.ts
+++ src/back-end/src/modules/agency/models/agency.params.ts
@@ -1,5 +1,5 @@
-import { PickType } from "@nestjs/swagger";
+import { PartialType, PickType } from "@nestjs/swagger";
 import { AgencyDto } from "./agency.dto";
 
-export class AgencyParams extends PickType( AgencyDto , ['dien_thoai' , 'email', 'ngay_tiep_nhan' ,'tien_no', 'ten' ]){
+export class AgencyParams extends PartialType( PickType( AgencyDto , ['dien_thoai' , 'email', 'ngay_tiep_nhan' ,'tien_no', 'ten' ])){
 }
