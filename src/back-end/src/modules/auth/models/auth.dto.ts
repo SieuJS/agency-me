ce07ef2211e37038c30c2af018362e00a5a4d990
Ddyln <ddlan99tn@gmail.com>
2025-05-07 21:26:50 +0700

    remove constructor

--- src/back-end/src/modules/auth/models/auth.dto.ts
+++ src/back-end/src/modules/auth/models/auth.dto.ts
@@ -1,6 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsEmail } from 'class-validator';
-import { NhanVien } from '@prisma/client';
 
 export class RegisterDto {
   @ApiProperty({ example: 'user@gmail.com' })
@@ -26,15 +25,6 @@ export class RegisterDto {
   @ApiProperty({ example: '123 Đường ABC, Quận 1, TP.HCM' })
   @IsString()
   diaChi: string;
-
-  constructor(dbInstance: NhanVien) {
-    this.email = dbInstance.email;
-    this.password = dbInstance.mat_khau;
-    this.ten = dbInstance.ten;
-    this.dienThoai = dbInstance.dien_thoai;
-    this.loaiNhanVienId = dbInstance.loai_nhan_vien_id;
-    this.diaChi = dbInstance.dia_chi;
-  }
 }
 
 export class LoginDto {
@@ -45,9 +35,4 @@ export class LoginDto {
   @ApiProperty({ example: 'password' })
   @IsString()
   password: string;
-
-  constructor(dbInstance: NhanVien) {
-    this.email = dbInstance.email;
-    this.password = dbInstance.mat_khau;
-  }
 }
