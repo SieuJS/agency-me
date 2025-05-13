0106709c497b655f1b1d0524d46b5b16c4d44abc
Ddyln <ddlan99tn@gmail.com>
2025-05-13 14:12:12 +0700

    add register and login response for auth module

--- src/back-end/src/modules/auth/models/auth-response.dto.ts
+++ src/back-end/src/modules/auth/models/auth-response.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { NhanVien } from '@prisma/client';
+import { LoginDto, RegisterDto } from './auth.dto';
 
 export class ProtectedResponseDto {
   @ApiProperty()
@@ -7,4 +8,34 @@ export class ProtectedResponseDto {
 
   @ApiProperty({ type: 'object', additionalProperties: true })
   user: NhanVien;
+}
+
+
+export class RegisterResponse {
+  @ApiProperty({
+    example: 'Registered successfully',
+    description: 'Message indicating the result of the operation'
+  })
+  message: string;
+
+  @ApiProperty({
+    type: RegisterDto,
+    description: 'The registered account.'
+  })
+  account: RegisterDto;
+}
+
+export class LoginResponse {
+  @ApiProperty({
+    example: 'Login successfully',
+    description: 'Message indicating the result of the operation'
+  })
+  message: string;
+
+  @ApiProperty({
+    type: LoginDto,
+    description: 'account info'
+  })
+  account: LoginDto;
+
 }
