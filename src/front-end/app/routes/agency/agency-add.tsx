63b48eccdfda8a674737fdf605c13bc9bd6ce50f
Minh Anh <22120213@student.hcmus.edu.vn>
2025-05-24 08:56:41 +0700

    Trang Lập phiếu xuất hàng

--- src/front-end/app/routes/agency/agency-add.tsx
+++ src/front-end/app/routes/agency/agency-add.tsx
@@ -192,7 +192,7 @@ ten: agencyName,
                       required
                       className={`flex-grow mt-1 block w-full rounded-md border ${errors.agencyType ? 'border-red-500' : 'border-gray-300'} shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2 h-[42px]`}
                     >
-                      <option value="" disabled>
+                      <option value="" disabled hidden>
                         {isAgencyTypesLoading ? 'Đang tải...' : '-- Chọn loại đại lý --'}
                       </option>
                       {!isAgencyTypesLoading && agencyTypesFromAPI.map((type) => (
@@ -244,7 +244,7 @@ ten: agencyName,
                     required
                     className={`mt-1 block w-full rounded-md border ${errors.district ? 'border-red-500' : 'border-gray-300'} shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2 h-[42px]`}
                   >
-                    <option value="" disabled>
+                    <option value="" disabled hidden>
                         {isDistrictsLoading ? 'Đang tải...' : '-- Chọn quận --'}
                     </option>
                     {!isDistrictsLoading && districtsFromAPI.map((dist) => (
