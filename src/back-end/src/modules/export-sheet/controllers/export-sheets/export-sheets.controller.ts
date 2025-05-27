b9038de13b55fba19dc98aa1fd48464884df8759
sieujs <gamefun206@gmail.com>
2025-05-27 20:07:39 +0700

    fix: linting

--- src/back-end/src/modules/export-sheet/controllers/export-sheets/export-sheets.controller.ts
+++ src/back-end/src/modules/export-sheet/controllers/export-sheets/export-sheets.controller.ts
@@ -1,5 +1,10 @@
 import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { ExportSheetsService } from '../../services/export-sheets/export-sheets.service';
 import { ExportSheetInput } from '../../models/export-sheet.input';
 import { AuthGuard } from '@nestjs/passport';
@@ -9,26 +14,27 @@ import { ExportSheetInputPipe } from '../../pipes/export-sheet-input.pipe';
 @ApiTags('Export Sheets')
 @Controller('export-sheets')
 export class ExportSheetsController {
-    constructor(private readonly exportSheetsService: ExportSheetsService) {}
+  constructor(private readonly exportSheetsService: ExportSheetsService) {}
 
-    @Post()
-    @ApiOperation({ summary: 'Create a new export sheet' })
-    @ApiResponse({
-        status: 201,
-        description: 'The export sheet has been successfully created.',
-    })
-    @ApiResponse({
-        status: 404,
-        description: 'Agency, employee, or item not found.',
-    })
-    @UseGuards(AuthGuard('jwt'))
-    @ApiBearerAuth('access-token')
-    async createExportSheet(@Body(ExportSheetInputPipe) input: ExportSheetInput, @Req() req: { user: AuthPayloadDto }) {
-        const user = req.user;
-        console.log(user);
+  @Post()
+  @ApiOperation({ summary: 'Create a new export sheet' })
+  @ApiResponse({
+    status: 201,
+    description: 'The export sheet has been successfully created.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Agency, employee, or item not found.',
+  })
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth('access-token')
+  async createExportSheet(
+    @Body(ExportSheetInputPipe) input: ExportSheetInput,
+    @Req() req: { user: AuthPayloadDto },
+  ) {
+    const user = req.user;
 
-        input.nhan_vien_lap_phieu = user.nhan_vien_id;
-        console.log(input);
-        return await this.exportSheetsService.createExportSheet(input);
-    }
+    input.nhan_vien_lap_phieu = user.nhan_vien_id as unknown as string;
+    return await this.exportSheetsService.createExportSheet(input);
+  }
 }
