e5b6659e4bf17d0a68bea19afc309b4057cca5df
MDuy-3107 <22120080@student.hcmus.edu.vn>
2025-06-19 18:11:58 +0700

    cập nhật lại giao diện và thay đổi đơn vị tính sang kí hiệu

--- src/front-end/app/routes/admin/changeRule/change-rule-debt.tsx
+++ src/front-end/app/routes/admin/changeRule/change-rule-debt.tsx
@@ -165,7 +165,7 @@ export default function RegulationDebtBatchPage() {
               </button>
               <button
                 onClick={onModalSave}
-                className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 disabled:opacity-50"
+                className="px-4 py-2 bg-black text-white rounded hover:bg-gray-800"
                 disabled={submitting}
               >
                 Lưu
